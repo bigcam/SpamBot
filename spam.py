2e55ee143330a42660804a97f7e9a478fc50137e
BuildTools <unconfigured@null.spigotmc.org>
2017-08-25 16:18:51 -0400

    initial commit

---
+++ spam.py
@@ -0,0 +1,56 @@
+
+import requests
+import discord
+import subprocess
+import threading
+import sys
+import os
+import asyncio
+
+client = discord.Client() # new discord client 
+
+
+
+if os.name == "nt":
+	os.system("cls")
+elif os.name == "posix":
+	os.system('clear')
+
+
+helpMessage = ("+++++++++++++++++++++++++++++++++\n"
+			   "+type 'help' to get help        +\n"
+			   "+type 'exit' to exit the script +\n"
+			   "type 'start' to start the attack+\n"
+			   "+++++++++++++++++++++++++++++++++"
+			   )
+
+@client.event
+async def on_ready():
+
+	while 1:
+		command = input(" # ")
+
+		if command == "start":
+			count = int(input("how many times: "))
+			spamText = input("what do you want to send? ")
+			uName = input("insert target name: ")
+			targetUser = [m for m in client.get_all_members() if m.name == uName]
+			print(targetUser)
+			if count > 0 and count <= 1000:
+				for i in range(count):
+					await client.send_message(targetUser[0], spamText)
+					await asyncio.sleep(1)
+
+		if command == "exit":
+			print("exiting script")
+			await client.logout()
+
+		if command == "help":
+			print(helpMessage)
+
+	return;
+
+
+
+
+client.run('MjI0NjA4MTM0NDU3NDU4Njg4.DEhHrg.tv_JFUhR36gRkb3fWiV2HV-51XM')  #bot token goes here 
